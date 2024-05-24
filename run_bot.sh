3770c7428dfd6fcd18249921bdfe92e87ebec24f
Vladislav Kurichenko <vkurichenko@Vladislavs-MacBook-Pro.local>
2024-05-24 09:08:05 +0300

    Add and upd files for bot deployment

--- run_bot.sh
+++ run_bot.sh
@@ -2,10 +2,10 @@
 export PYTHONPATH=$(pwd)
 export OPENAI_API_KEY=''
 export BOT_TOKEN=''
-export db_host=''
-export db_name=''
+export db_host='0.0.0.0'
+export db_name='db'
 export db_port=5432
-export db_user=''
-export db_pwd=''
+export db_user='user'
+export db_pwd='password'
 
 python src/bot/bot.py
